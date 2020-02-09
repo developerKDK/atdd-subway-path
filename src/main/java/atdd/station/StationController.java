32c180c6fccd006f75d8a5cfba7397fd3a7d2671
DKKang <akwdmfxu@hanmail.net>
2020-02-09 12:51:18 +0900

    step0 커밋

--- src/main/java/atdd/station/StationController.java
+++ src/main/java/atdd/station/StationController.java
@@ -2,21 +2,28 @@ package atdd.station;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.Map;
 
 @RestController
 public class StationController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(StationController.class);
 
-    @PostMapping(value = "/stations")
-    public ResponseEntity createStation() {
-        return ResponseEntity.created(URI.create("/station/1")).build();
+    @PostMapping(value = "/stations", produces = "application/json")
+    @ResponseBody
+    public ResponseEntity<String> createStation(@RequestBody Map<String, String> param) {
+
+        String stationName = param.get("name");
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Location", stationName);
+
+        return ResponseEntity.ok().headers(httpHeaders).body("{\"name\":\""+stationName+"\"}");
     }
 
 }
--- src/test/java/atdd/station/StationAcceptanceTest.java
+++ src/test/java/atdd/station/StationAcceptanceTest.java
@@ -5,13 +5,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWebClient
+@AutoConfigureWebTestClient
 public class StationAcceptanceTest {
     private static final Logger logger = LoggerFactory.getLogger(StationAcceptanceTest.class);
 
@@ -27,7 +30,10 @@ public class StationAcceptanceTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(Mono.just(inputJson), String.class)
                 .exchange()
-                .expectStatus().isCreated();
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectHeader().exists("Location")
+                .expectBody().jsonPath("$.name").isEqualTo(stationName);
     }
 
 }
